8503b9afafc0709315248843fe3169b3280b7f6b
Brendan Nee <brendan@blinktag.com>
2019-08-06 22:52:30 -0700

    Title

--- pages/index.js
+++ pages/index.js
@@ -400,6 +400,7 @@ function Home() {
   return (
     <div>
       <Head>
+        <title>GTFS-to-HTML: Build human readable transit timetables in HTML from GTFS</title>
         <link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
         <link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/static/apple-touch-icon.png" sizes="180x180" />
