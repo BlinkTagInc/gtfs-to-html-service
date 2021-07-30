40eaa304448165b7ea999159bd42da60af2af49f
Annie Fifer <annie@trilliumtransit.com>
2021-07-30 11:52:56 -0700

    add request data and error handling

--- api/createTimetables.js
+++ api/createTimetables.js
@@ -1,51 +1,72 @@
+const Boom = require('@hapi/boom')
 const { v4: uuidv4 } = require('uuid')
+const fs = require('fs-extra')
+const path = require('path')
 const { createTimetablesSocketless } = require('../util/create');
 
+const defaultTemplate = 'default'
+const defaultOptions = {
+  allowEmptyTimetables: false,
+  beautify: false,
+  coordinatePrecision: 5,
+  dateFormat: 'MMM D, YYYY',
+  daysShortStrings: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  daysStrings: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  defaultOrientation: 'vertical',
+  interpolatedStopSymbol: '•',
+  interpolatedStopText: 'Estimated time of arrival',
+  linkStopUrls: true,
+  menuType: 'jump',
+  noDropoffSymbol: '‡',
+  noDropoffText: 'No drop off available',
+  noHead: false,
+  noPickupSymbol: '**',
+  noPickupText: 'No pickup available',
+  noServiceSymbol: '-',
+  noServiceText: 'No service at this stop',
+  outputFormat: 'html',
+  requestDropoffSymbol: '†',
+  requestDropoffText: 'Must request drop off',
+  requestPickupSymbol: '***',
+  requestPickupText: 'Request stop - call for pickup',
+  serviceNotProvidedOnText: 'Service not provided on',
+  serviceProvidedOnText: 'Service provided on',
+  showArrivalOnDifference: 0.2,
+  showMap: false,
+  showOnlyTimepoint: false,
+  showRouteTitle: true,
+  showStopCity: false,
+  showStopDescription: false,
+  sortingAlgorithm: 'common',
+  timeFormat: 'h:mma',
+  useParentStation: true,
+};
+
 module.exports = async (request, h) => {
-  const defaultOptions = {
-    allowEmptyTimetables: false,
-    beautify: false,
-    coordinatePrecision: 5,
-    dateFormat: 'MMM D, YYYY',
-    daysShortStrings: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    daysStrings: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    defaultOrientation: 'vertical',
-    interpolatedStopSymbol: '•',
-    interpolatedStopText: 'Estimated time of arrival',
-    linkStopUrls: true,
-    menuType: 'jump',
-    noDropoffSymbol: '‡',
-    noDropoffText: 'No drop off available',
-    noHead: false,
-    noPickupSymbol: '**',
-    noPickupText: 'No pickup available',
-    noServiceSymbol: '-',
-    noServiceText: 'No service at this stop',
-    outputFormat: 'html',
-    requestDropoffSymbol: '†',
-    requestDropoffText: 'Must request drop off',
-    requestPickupSymbol: '***',
-    requestPickupText: 'Request stop - call for pickup',
-    serviceNotProvidedOnText: 'Service not provided on',
-    serviceProvidedOnText: 'Service provided on',
-    showArrivalOnDifference: 0.2,
-    showMap: false,
-    showOnlyTimepoint: false,
-    showRouteTitle: true,
-    showStopCity: false,
-    showStopDescription: false,
-    sortingAlgorithm: 'common',
-    timeFormat: 'h:mma',
-    useParentStation: true,
-  };
-    
-  const buildId = uuidv4();
+  const { options, url, template = defaultTemplate } = request.payload
+
+  if (!/^(f|ht)tps?:\/\//i.test(url)) {
+    throw Boom.badRequest('Invalid URL')
+  }
+  
+  const templatePath = path.join(process.env.TEMPLATE_DIR, template + '.json')
+
+  if (!fs.realpathSync(templatePath).startsWith(process.env.TEMPLATE_DIR)) {
+    throw Boom.badRequest('Invalid template')
+  }
+  
+  if (!fs.existsSync(templatePath)) {
+    throw Boom.notFound('Template not found')
+  }
 
   const data = {
-    // For testing
-    url: "https://data.trilliumtransit.com/gtfs/petalumatransit-petaluma-ca-us/petalumatransit-petaluma-ca-us--ttable.zip",
-    buildId,
-    options: defaultOptions
+    url,
+    buildId: uuidv4(),
+    options: {
+      ...defaultOptions,
+      ...options,
+      templatePath,
+    }
   }
 
   return await createTimetablesSocketless(data)
