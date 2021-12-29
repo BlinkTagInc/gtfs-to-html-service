323fa6371626844d9f502320aea0f8e81c338ebd
Annie Fifer <annie@trilliumtransit.com>
2021-12-28 17:20:40 -0800

    max_old_space_size

--- pm2.config.js
+++ pm2.config.js
@@ -2,6 +2,6 @@ module.exports = {
   apps : [{
     name      : 'gtfs-to-html-service',
     script    : 'server.mjs',
-    node_args : '-r dotenv/config'
+    node_args : ['-r dotenv/config', '--max_old_space_size=4096']
   }]
 }
