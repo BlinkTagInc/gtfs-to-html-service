a91a5ba89582fcb126976c2dd5acb307457b46d6
Annie Fifer <annie@trilliumtransit.com>
2021-12-30 14:24:48 -0800

    fix node args

--- pm2.config.js
+++ pm2.config.js
@@ -2,6 +2,6 @@ module.exports = {
   apps : [{
     name      : 'gtfs-to-html-service',
     script    : 'server.mjs',
-    node_args : ['-r dotenv/config', '--max_old_space_size=4096']
+    node_args : '-r dotenv/config'
   }]
 }
